e948649ec202a944742bca2e110bfdca14f4e535
TalkoDA <azzi2710@gmail.com>
2026-05-11 10:56:58 +0200

    Added base.py

---
+++ src/base.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import List, Optional
+import datetime
+
+@dataclass
+class UpliftResult:
+    total_uplift: float
+    relative_uplift_percentage: float
+    train_mae: float
+    test_dates: List[datetime.date]
+    forecast_data: List[float]
+
+    def plot:
+        pass
+
+class BaseForecaster(ABC):
+
+    @abstractmethod
+    def fit(self, x: List[int], y:List[float]) -> None:
+        pass
+
+    @abstractmethod
+    def predict(self, x: List[int]) -> List[float]:
+        pass
